f61a4a34df1425959728365c8f4e5ce4d8b2197c
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-05-04 13:42:59 -0400

    overall page style adjustments

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import Header from './components/Header/index'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+   
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import App from "./App";
+// import Users from "./users";
+// import Contact from "./contact";
+// import Notfound from "./notfound";
+import Header from "./components/Header/index";
+import Footer from "./components/Footer/index";
+
+const routing = (
+  <Router>
+    <div>
+      <Header />
+      <hr />
+      <Switch>
+        <Route exact path="/" component={App} />
+        {/* <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+        <Route component={Notfound} /> */}
+      </Switch>
+      <hr />
+      <Footer />
+    </div>
+  </Router>
 );
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 
