ef8dee602c50d6c217462ff647c8626976e64353
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-05-07 09:36:35 -0400

    tweaking routes. they still don't work very well, but i'm not sure why.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import React from "react";
+import "./index.css";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar/index";
+import Footer from "./components/Footer/index";
+import Home from "./components/Header/index";
+import Projects from "./components/Projects/CodingGallery/Gallery";
+import PDF from "./components/Projects/TechWriteGallery/viewPDF";
+// import Doc from "./components/Projects/viewGuide";
+
 
 function App() {
   return (
-    <div className="App">
-          portfolio!    
-    </div>
+    <Router>
+    <Navbar />
+
+    <Switch>
+      <Route exact path={["/", "/home"]}><Home /></Route>
+      <Route path="/work"><Projects /></Route>
+      <Route path="/docs"><PDF /></Route>
+      <Route path="/resume" ><PDF /></Route>
+    </Switch>
+
+    <Footer />
+   
+</Router>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import Navbar from "./components/Navbar/index";
-import Footer from "./components/Footer/index";
-import Header from "./components/Header/index";
-import Gallery from "./components/Projects/projectGallery.js";
-import pdfDoc from "./components/Projects/index";
+import App from "./App";
 
-const routing = (
-  <Router>
-    <div>
-      <Navbar />
-    </div>
-    <div>
-      <Switch>
-        <Route exact path="/" component={Header} />
-        <Route path="react-portfolio/home" component={Header} />
-        <Route path="react-portfolio/work" component={Gallery} />
-        <Route path="react-portfolio/resume" component={pdfDoc} />
-      </Switch>
-      <Footer />
-    </div>
-  </Router>
-);
-
-ReactDOM.render(routing, document.getElementById("root"));
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
