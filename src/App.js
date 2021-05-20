bf4b8c3b6bc9bb6c123c022a03ed41328b4b4bd7
Amanda Hamilton <the.a.hamilton@gmail.com>
2021-05-20 14:10:27 -0400

    upgrading project gallery to  dynamically render cards with props

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
 import React from "react";
 import "./index.css";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route, HashRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar/index";
 import Footer from "./components/Footer/index";
 import Home from "./components/Header/index";
-import Projects from "./components/Projects/CodingGallery/Gallery";
+import Projects from "./components/Card/projectCard";
 import PDF from "./components/Projects/TechWriteGallery/viewPDF";
-// import Doc from "./components/Projects/viewGuide";
-
 
 function App() {
   return (
     <Router>
-    <Navbar />
-
-    <Switch>
-      <Route exact path={["/", "/home"]}><Home /></Route>
-      <Route path="/work"><Projects /></Route>
+      <Navbar />
+      <Route exact path={"/"}><Home /></Route>
+      <Route path={"/home"}><Home /></Route>
+      <Route exact path="/work"><Projects /></Route>
       <Route path="/docs"><PDF /></Route>
       <Route path="/resume" ><PDF /></Route>
-    </Switch>
-
-    <Footer />
-   
-</Router>
+      <Footer />
+    </Router>
 
   );
 }
 
 export default App;
-
-
---
+++ src/components/Card/projectCard.js
@@ -0,0 +1,93 @@
+import React from "react";
+import Card from "../Projects/CodingGallery/Gallery";
+let babysittr = require("../../images/babysittr.png")
+let fungID = require("../../images/fungID.png")
+let profileGen = require("../../images/profileGen.png")
+let pwGen = require("../../images/pwGen.png")
+let python = require("../../images/python.png")
+let neighborly = require("../../images/neighborly.png")
+
+const CardData = [
+    {
+        image: neighborly,
+        alt: "screenshot",
+        title: "Neighborly",
+        desc: "it thing",
+        urlApp: "https://infinite-forest-58133.herokuapp.com/",
+        urlRepo: "https://github.com/polysnacktyl/project3/tree/amanda"
+    },
+    {
+        image: fungID,
+        alt: "screenshot", 
+        title: "fungID",
+        desc: "a fun fungus-finder's guide", 
+        urlApp: "https://fung-i-d-ployed.herokuapp.com/mush-room",
+        urlRepo: "https://github.com/polysnacktyl/project-1/tree/amanda"
+       
+    },
+    {
+        image: profileGen,
+        alt: "screenshot",
+        title:"node.js profile generator",
+        desc:"",
+        urlApp: "",
+        urlRepo: "https://github.com/polysnacktyl/team-profile-generator"
+      
+
+    }, 
+    {
+        image: pwGen,
+        alt: "screenshot",
+        title:"password generator",
+        desc:"a javascript password generator",
+        urlApp: "https://polysnacktyl.github.io/js-password-generator/",
+        urlRepo: "https://github.com/polysnacktyl/js-password-generator"
+
+    }, 
+    {
+        image: babysittr,
+        alt: "screenshot",
+        title:"babysittr",
+        desc:"with the skills we had at the time, we built what we could of an idea for helping parents and sitters communicate the minutia of childcare",
+        urlApp: "https://polysnacktyl.github.io/project-1/",
+        urlRepo: "https://github.com/polysnacktyl/project-1/tree/amanda"
+
+    }, 
+    {
+        image: python,
+        alt: "screenshot",
+        title:"python script guide ",
+        desc:"a first-timer's guide to compiling a script that helps massage CSV data between documents",
+        urlApp: "",
+        urlRepo: ""
+    }, 
+]
+
+function Projects() {
+    return (
+        <body>
+            <div className="container">
+                <div className="card-deck">
+                    {CardData.map((item) => {
+                        return (
+                            <Card
+                                image={item.image}
+                                alt={item.alt}
+                                title={item.title}
+                                desc={item.desc}
+                                urlApp={item.urlApp}
+                                urlRepo={item.urlRepo}
+                              
+                                
+                            />
+                        );
+                    })}
+                </div>
+            </div>
+        </body>
+
+
+    )
+};
+
+export default Projects;
