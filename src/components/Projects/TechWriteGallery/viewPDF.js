c21ad67178136e9337152cf0b69bf8bab251358c
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-05-23 11:02:06 -0700

    content container styling for under construction pages

--- src/components/Projects/TechWriteGallery/viewPDF.js
+++ src/components/Projects/TechWriteGallery/viewPDF.js
@@ -1,15 +1,13 @@
 import React from "react";
+import "./style.css";
 
 class PDF extends React.Component {
     render() {
         return (
-                  <div className="holdPlease">
-            
-        
-       <p> stay tuned for the beautiful future in which this page features a fully functional PDF-viewing experience.</p>
-       
-     </div>
-            
+            <div className="content-container">
+                <p id="holdPlease"> stay tuned for the beautiful future in which this page features a fully functional PDF-viewing experience.</p>
+            </div>
+
         );
     }
 
