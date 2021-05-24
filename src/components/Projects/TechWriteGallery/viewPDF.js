edb532c8ce741e278e0450cb9e50318bfee8e8be
Amanda Hamilton <77996281+polysnacktyl@users.noreply.github.com>
2021-05-23 18:05:23 -0700

    style tweaks

--- src/components/Projects/TechWriteGallery/viewPDF.js
+++ src/components/Projects/TechWriteGallery/viewPDF.js
@@ -5,7 +5,7 @@ class PDF extends React.Component {
     render() {
         return (
             <div className="content-container">
-                <p id="holdPlease"> stay tuned for the beautiful future in which this page features a fully functional PDF-viewing experience.</p>
+                <p> stay tuned for the beautiful future in which this page features a fully functional PDF-viewing experience.</p>
             </div>
 
         );
